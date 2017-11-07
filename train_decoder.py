61a985a8c4eda924094beebf0e16b2b6a706c636
Abhijith <abhijith97@yahoo.co.in>
2017-11-08 04:22:02 +0530

    Add fit function for training

--- train_decoder.py
+++ train_decoder.py
@@ -11,7 +11,7 @@ import warnings
 from keras.models import Model
 from keras.layers import Flatten, Dense, Input
 from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
+from keras.layers import MaxPooling2D, UpSampling2D
 from keras.layers import GlobalMaxPooling2D
 from keras.layers import GlobalAveragePooling2D
 from keras.preprocessing import image
@@ -31,7 +31,7 @@ WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases
 def VGG19(include_top=True, weights='imagenet',
           input_tensor=None, input_shape=None,
           pooling=None,
-          classes=1000):
+          classes=1000, name="vgg19_1"):
     """Instantiates the VGG19 architecture.
     Optionally loads weights pre-trained
     on ImageNet. Note that when using TensorFlow,
@@ -89,7 +89,7 @@ def VGG19(include_top=True, weights='imagenet',
                                       default_size=224,
                                       min_size=48,
                                       data_format=K.image_data_format(),
-                                      include_top=include_top)
+                                      require_flatten=include_top)
 
     if input_tensor is None:
         img_input = Input(shape=input_shape)
@@ -148,7 +148,131 @@ def VGG19(include_top=True, weights='imagenet',
     else:
         inputs = img_input
     # Create model.
-    model = Model(inputs, x, name='vgg19')
+    model = Model(inputs, x, name=name)
+
+    # load weights
+    if weights == 'imagenet':
+        if include_top:
+            weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels.h5',
+                                    WEIGHTS_PATH,
+                                    cache_subdir='models')
+        else:
+            weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                                    WEIGHTS_PATH_NO_TOP,
+                                    cache_subdir='models')
+        model.load_weights(weights_path)
+        if K.backend() == 'theano':
+            layer_utils.convert_all_kernels_in_model(model)
+
+        if K.image_data_format() == 'channels_first':
+            if include_top:
+                maxpool = model.get_layer(name='block5_pool')
+                shape = maxpool.output_shape[1:]
+                dense = model.get_layer(name='fc1')
+                layer_utils.convert_dense_weights_data_format(dense, shape, 'channels_first')
+
+            if K.backend() == 'tensorflow':
+                warnings.warn('You are using the TensorFlow backend, yet you '
+                              'are using the Theano '
+                              'image data format convention '
+                              '(`image_data_format="channels_first"`). '
+                              'For best performance, set '
+                              '`image_data_format="channels_last"` in '
+                              'your Keras config '
+                              'at ~/.keras/keras.json.')
+    # print model.summary()
+    return model
+
+def DECODER(include_top=True, weights='imagenet',
+          input_tensor=None, input_shape=None,
+          pooling=None,
+          classes=1000):
+    """Instantiates the VGG19 architecture.
+    Optionally loads weights pre-trained
+    on ImageNet. Note that when using TensorFlow,
+    for best performance you should set
+    `image_data_format="channels_last"` in your Keras config
+    at ~/.keras/keras.json.
+    The model and the weights are compatible with both
+    TensorFlow and Theano. The data format
+    convention used by the model is the one
+    specified in your Keras config file.
+    # Arguments
+        include_top: whether to include the 3 fully-connected
+            layers at the top of the network.
+        weights: one of `None` (random initialization)
+            or "imagenet" (pre-training on ImageNet).
+        input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
+            to use as image input for the model.
+        input_shape: optional shape tuple, only to be specified
+            if `include_top` is False (otherwise the input shape
+            has to be `(224, 224, 3)` (with `channels_last` data format)
+            or `(3, 224, 244)` (with `channels_first` data format).
+            It should have exactly 3 inputs channels,
+            and width and height should be no smaller than 48.
+            E.g. `(200, 200, 3)` would be one valid value.
+        pooling: Optional pooling mode for feature extraction
+            when `include_top` is `False`.
+            - `None` means that the output of the model will be
+                the 4D tensor output of the
+                last convolutional layer.
+            - `avg` means that global average pooling
+                will be applied to the output of the
+                last convolutional layer, and thus
+                the output of the model will be a 2D tensor.
+            - `max` means that global max pooling will
+                be applied.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True, and
+            if no `weights` argument is specified.
+    # Returns
+        A Keras model instance.
+    # Raises
+        ValueError: in case of invalid argument for `weights`,
+            or invalid input shape.
+    """
+    img_input = Input(shape=(7, 7,512))
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='dblock5_conv4')(img_input)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='dblock5_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='dblock5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='dblock5_conv1')(x)
+    x = UpSampling2D((2, 2), name='dblock5_pool')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='dblock4_conv4')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='dblock4_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='dblock4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='dblock4_conv1')(x)
+    x = UpSampling2D((2, 2), name='dblock4_pool')(x)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='dblock3_conv4')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='dblock3_conv3')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='dblock3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='dblock3_conv1')(x)
+    x = UpSampling2D((2, 2), name='dblock3_pool')(x)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='dblock2_conv2')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='dblock2_conv1')(x)
+    x = UpSampling2D((2, 2),  name='dblock2_pool')(x)
+
+    # Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='dblock1_conv2')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='dblock1_conv1')(x)
+    x = UpSampling2D((2, 2),  name='dblock1_pool')(x)
+    x = Conv2D(3, (3, 3), activation='relu', padding='same', name='dblock0_conv1')(x)
+
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+    # Create model.
+    model = Model(inputs, x, name='decoder')
 
     # load weights
     if weights == 'imagenet':
@@ -184,26 +308,38 @@ def VGG19(include_top=True, weights='imagenet',
 
 
 if __name__ == '__main__':
-    model_decoder = VGG19(include_top=True, weights='imagenet') #Change to decoder model
-    model_VGG_2 = VGG19(include_top=True, weights='imagenet')
-
-    inputs = Input(shape=(784,))
-    decode_out = model_decoder()(inputs)
-    vgg2_out = model_VGG_2()(decode_out)
+    model_decoder = DECODER(include_top=False, weights=None, input_shape=(7, 7, 512)) #Change to decoder model
+    model_VGG_2 = VGG19(include_top=False, weights='imagenet', input_shape=(224, 224, 3) , name="vgg19_2")
+    # i.e. freeze all convolutional layers
+    for layer in model_VGG_2.layers:
+        layer.trainable = False
+    print(model_VGG_2.summary())
+    print(model_decoder.summary())
+    inputs = Input(shape=(7, 7, 512))
+    decode_out = model_decoder(inputs)
+    vgg2_out = model_VGG_2(decode_out)
 
     model = Model(inputs=[inputs], outputs=[decode_out, vgg2_out])
-#    print model.summary()
-    # model_VGG_1 = VGG19(include_top=False, weights='imagenet')
 
-#     img_path = 'cat.jpg'
-#     img = image.load_img(img_path, target_size=(224, 224))
-#     x = image.img_to_array(img)
-#     x = np.expand_dims(x, axis=0)
-#     x = preprocess_input(x)
-#     print('Input image shape:', x.shape)
 
-#     main_input = Input(shape=(100,), dtype='int32', name='main_input')
-#     VGG_1_out = VGG_1.predict(main_input)
+
+    model.compile(optimizer='rmsprop',
+              loss={'decoder': 'mean_squared_error', 'vgg19_2': 'mean_squared_error'},
+              loss_weights={'decoder': 1., 'vgg19_2': 1.})
+
+
+    print(model.summary())
+    model_VGG_1 = VGG19(include_top=False, weights='imagenet', name="vgg19_1")
+
+    img_path = 'style.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    print('Input image shape:', x.shape)
+
+    VGG_1_out = model_VGG_1.predict(x)
+    print(VGG_1_out)
 
 
 
