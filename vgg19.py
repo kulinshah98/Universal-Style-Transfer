13ec625fd09c5f9a9353ba16fbc31f4ead9ac4cc
Tirth Maniar <tirthmaniar1998@gmail.com>
2017-11-11 15:15:46 +0530

    Changes saved

--- vgg19.py
+++ vgg19.py
@@ -98,7 +98,7 @@ def VGG19(include_top=True, weights='imagenet',
                                       default_size=224,
                                       min_size=48,
                                       data_format=K.image_data_format(),
-                                      require_flatten=include_top)
+                                      include_top=include_top)
 
     if input_tensor is None:
         img_input = Input(shape=input_shape)
